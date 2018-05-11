1ea1ccaa8fc954fc74e6dceb35dbdb8838dabc41
Kanmani-Karthikeyan <39095358+Kanmani-Karthikeyan@users.noreply.github.com>
2018-05-11 10:46:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+	<title>Login page</title>
+	 </head>
+  <body>
+	<FORM ACTION=process_form.php METHOD=POST>
+	 <style style="text/css">
+	 
+    body {
+  background-image: url("https://www.svce.ac.in/theme/svce.png");
+  background-position: 4% 75%;
+  background-repeat: repeat;
+       }
+	   </style></br>
+	   
+	  </br></br></br></br></br></br></br>
+	  
+       <h1><center><font color="black">Login page:</font></h1></center>
+	  
+	  </br></br></br>
+	  <center><b><h1><font color="black">NAME: <INPUT TYPE=TEXT NAME="realname"></font><BR></h1></b></center> 
+	  <center><b><h1><font color="black">PASSWORD: <INPUT TYPE=PASSWORD NAME="mypassword"></font></h1> </b></center>
+	  <center><P><INPUT TYPE=SUBMIT VALUE="SUBMIT"></center>
+	</FORM>
+  </body>
+</html>
